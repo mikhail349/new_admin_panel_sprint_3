e567a16dbd8beec2fb317f8386fa923b74d10d36
mikhail349 <mikhail349349@gmail.com>
2022-10-20 11:04:11 +0300

    ENV DB_HOST и DB_PORT из app/Dockerfile вынесены в переменные окружения

--- app/commands/start.sh
+++ app/commands/start.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+[[ -z "$DB_HOST" ]] && { echo "Parameter DB_HOST is empty" ; exit 1; }
+[[ -z "$DB_PORT" ]] && { echo "Parameter DB_PORT is empty" ; exit 1; }
+
 while ! nc -z $DB_HOST $DB_PORT; do
     sleep 0.1
 done
