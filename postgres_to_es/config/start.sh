0b99f3b0f8e9b76a1510a10b3590e56533eee0a2
mikhail349 <mikhail349349@gmail.com>
2022-10-20 10:13:31 +0300

    Revert "Добавлена инициализация индекса"
    This reverts commit 794e410c3f616fd63c5ac8243f1638a97dac821b.

--- postgres_to_es/config/start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-while ! nc -z $ES_HOST $ES_PORT; do
-    sleep 1
-done
-
-curl -XPUT $ES_HOST:$ES_PORT/$ES_INDEX -H 'Content-Type: application/json' -d @config/es_index.json
-python app/main.py
